e2d351711c001941968356263e610bd76fe3b453
Ye Yukang <yeyukang@idr.ai>
2023-02-25 00:34:59 +0800

    [MOD]:Final version

--- os/code/kernel.c
+++ os/code/kernel.c
@@ -6,10 +6,6 @@ extern void os_main(void);
 void start_kernel(void)
 {
 
-    asm volatile("li s7,  1"
-                 :
-                 :);
-    for (int i = 5000; i > 0; i--);
     os_main();
 
     while (1)
--- os/code/user.c
+++ os/code/user.c
@@ -3,15 +3,93 @@
 
 void alg0()
 {
-    asm volatile("li s8,  4"
-                 :
-                 :);
+    int t = 20;
+    while (t > 0){
+        t--;
+    }
+    // #define V 6
+    // int start = 0;
+    // int res = {0, 3, 4, 5, 4};
+
+    // // 定义邻接矩阵
+    
+    // int graph[V][V] = {
+    //     {0, 2, 0, 0, 0, 1},
+    //     {2, 0, 4, 1, 0, 0},
+    //     {0, 4, 0, 1, 3, 0},
+    //     {0, 1, 1, 0, 2, 0},
+    //     {0, 0, 3, 2, 0, 1},
+    //     {1, 0, 0, 0, 1, 0}
+    // };
+
+    // // 用于存储最短路径的数组
+    // int dist[V];
+
+    // // 记录哪些顶点已经处理过
+    // int processed[V];
+
+    // // 初始化距离和处理标记数组
+    // for (int i = 0; i < V; i++) {
+    //     dist[i] = INT_MAX;
+    //     processed[i] = 0;
+    // }
+
+    // // 将起始顶点到自身的距离初始化为0
+    // dist[start] = 0;
+
+    // // 处理剩余V-1个顶点
+    // for (int i = 0; i < V - 1; i++) {
+    //     // 找到距离起点最近的未处理顶点
+    //     int min_dist = INT_MAX;
+    //     int min_dist_index;
+    //     for (int j = 0; j < V; j++) {
+    //         if (processed[j] == 0 && dist[j] <= min_dist) {
+    //             min_dist = dist[j];
+    //             min_dist_index = j;
+    //         }
+    //     }
+
+    //     // 标记该顶点已经处理过
+    //     processed[min_dist_index] = 1;
+
+    //     // 更新与该顶点相邻的顶点的距离
+    //     for (int j = 0; j < V; j++) {
+    //         if (graph[min_dist_index][j] != 0 &&
+    //             processed[j] == 0 &&
+    //             dist[min_dist_index] != INT_MAX &&
+    //             dist[min_dist_index] + graph[min_dist_index][j] < dist[j]) {
+    //             dist[j] = dist[min_dist_index] + graph[min_dist_index][j];
+    //         }
+    //     }
+    // }
+
+    // int count = 0;
+    // for (int i = 0; i < V; i++) {
+    //     if (dist[i] == res[i]) {
+    //         count++;
+    //     }
+    // }
+
+    // if (count == V) {
+    //     return;
+    // } 
+    // else {
+    //     asm volatile("li s9,  1" : :);
+    // }
+
+    // // 输出结果
+    // printf("顶点\t最短距离\n");
+    // for (int i = 0; i < V; i++) {
+    //     printf("%d\t\t%d\n", i, dist[i]);
+    // }
+
 }
 
 void gcd()
 {
     int n = 5;
     int arr[n];
+    int res = 3;
     arr[0] = 6;
     arr[1] = 15;
     arr[2] = 9;
@@ -30,15 +108,22 @@ void gcd()
         }
         result = a;
     }
+    if (result == res) {
+        return;
+    }
+    else {
+        asm volatile("li s8,  1" : :);
+    }
     // printf("数组中的最大公约数为：%d\n", result);
-    asm volatile("li s8,  5"
-                 :
-                 :);
+    // asm volatile("li s8,  5"
+    //              :
+    //              :);
 }
 
 void quicksort()
 {
     int arr[] = {5, 3, 8, 4, 2, 7, 1, 10};
+    int res[] = { 1, 2, 3, 4, 5, 7, 8, 10};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     int left = 0;
@@ -81,41 +166,43 @@ void quicksort()
         }
     }
 
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == res[i]) {
+            count++;
+        }
+    }
+
+    if (count == n) {
+        return;
+    } 
+    else {
+        asm volatile("li s8,  1" : :);
+        asm volatile("li s9,  1" : :);
+    }
+
     // printf("排序后的数组：");
     // for (int i = 0; i < n; i++) {
     //     printf("%d ", arr[i]);
     // }
-    asm volatile("li s8,  6"
-                 :
-                 :);
+    // asm volatile("li s8,  6"
+    //              :
+    //              :);
 }
 
 /* NOTICE: DON'T LOOP INFINITELY IN main() */
 void os_main(void)
-{
+{   
+    asm volatile("li s7,  1"
+                 :
+                 :);
     alg0();
-    asm volatile("li s8,  1" : :);
-    asm volatile("li s9,  1" : :);
-    for (int i = 5000; i > 0; i--);
-    asm volatile("li s8,  0" : :);
-    asm volatile("li s9,  0" : :);
-    for (int i = 5000; i > 0; i--);
 
     gcd();
-    asm volatile("li s8,  1" : :);
-    asm volatile("li s9,  1" : :);
-    for (int i = 5000; i > 0; i--);
-    asm volatile("li s8,  0" : :);
-    asm volatile("li s9,  0" : :);
-    for (int i = 5000; i > 0; i--);
 
     quicksort();
-    asm volatile("li s8,  1" : :);
-    asm volatile("li s9,  1" : :);
-    for (int i = 5000; i > 0; i--);
-    asm volatile("li s8,  0" : :);
-    asm volatile("li s9,  0" : :);
-    for (int i = 5000; i > 0; i--);
 
-    asm volatile("li s10,  1" : :);
+    asm volatile("li s10,  1" 
+                 : 
+                 :);
 }
